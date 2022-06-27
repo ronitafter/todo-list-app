32ae54a0b0af2f5ef7759a7af4721ff84abe53da
ronitafter <54575125+ronitafter@users.noreply.github.com>
2022-06-27 12:08:03 +0300

    state

--- src/App.js
+++ src/App.js
@@ -1,55 +1,70 @@
-import Header from './Header';
-import Content from './Content';
-import Footer from './Footer';
-import { useState } from 'react';
-
+import Header from "./Header";
+import SearchItem from "./SearchItem";
+import AddItem from "./AddItem";
+import Content from "./Content";
+import Footer from "./Footer";
+import { useState } from "react";
 
 function App() {
-	const [items, setItems] = useState([
-		{
-			id: 1,
-			checked:false,
-			item: 'sushi'
-	},
-		{
-			id: 2,
-			checked:false,
-			item: 'coffee'
-	},
-	{
-			id: 3,
-			checked:false,
-			item: 'chocolate'
-	},
-]);
+  const [items, setItems] = useState(
+    JSON.parse(localStorage.getItem("todolist"))
+  );
 
-  const handleCheck = (id) =>{
-      const listItems = items.map((item)=> item.id === id ? 
-      {...item, checked: ! item.checked } : item);
-      setItems(listItems);
-      localStorage.setItem('todolist', JSON.stringify(listItems));
-  }
+  const [newItem, setNewItem] = useState("");
+  const [search, setSearch] = useState("");
+  const setAndSaveItems = (newItems) => {
+    setItems(newItems);
+    localStorage.setItem("todolist", JSON.stringify(newItems));
+  };
+
+  const addItem = (item) => {
+    const id = items.length ? items[items.length - 1].id + 1 : 1;
+    const myNewItem = { id, checked: false, item };
+    const listItems = [...items, myNewItem];
+    setAndSaveItems(listItems);
+  };
+  const handleCheck = (id) => {
+    const listItems = items.map((item) =>
+      item.id === id ? { ...item, checked: !item.checked } : item
+    );
+    setAndSaveItems(listItems);
+  };
 
   const handleDelete = (id) => {
-      const listItems = items.filter((item) =>item.id !== id);
-      setItems(listItems);
-      localStorage.setItem('todolist', JSON.stringify(listItems));	
-  }
-    
+    const listItems = items.filter((item) => item.id !== id);
+    setAndSaveItems(listItems);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (!newItem) return;
+    console.log(newItem);
+    addItem(newItem);
+    setNewItem("");
+  };
+
   return (
     <div className="App">
-      <Header title="To Do List"/>
-      <Content
-      items = {items}
-      handleCheck={handleCheck}
-      handleDelete={handleDelete}
+      <Header title="To Do List" />
+      <AddItem
+        newItem={newItem}
+        setNewItem={setNewItem}
+        handleSubmit={handleSubmit}
       />
-      <Footer length={items.length}/>
+      <SearchItem search={search} setSearch={setSearch} />
+      <Content
+        items={items.filter((item) =>
+          item.item.toLowerCase().includes(search.toLowerCase())
+        )}
+        handleCheck={handleCheck}
+        handleDelete={handleDelete}
+      />
+      <Footer length={items.length} />
     </div>
   );
 }
 
 Header.defaultProps = {
-  title: "default title"
-}
+  title: "default title",
+};
 export default App;
